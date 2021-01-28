fd492aeb5e708257e3182fc86f9439736ded60c2
Evan Armstrong <earmstrong@lcs.on.ca>
2021-01-28 10:01:19 -0500

    Solved the trident puzzle

--- Trident/main.swift
+++ Trident/main.swift
@@ -25,6 +25,12 @@ print("Enter tine spacing:")
 print("   ", terminator: "")
 let s = Int(readLine()!)!
 
+
+print("Enter Handle Length")
+print("   ", terminator: "")
+let h = Int(readLine()!)!
+
+
 // OUTPUT
 
 // draw all the tines!
@@ -39,14 +45,29 @@ for _ in 1...3 {
 
     // print the spaces
     for _ in 1...s{
-        print("s",terminator:"")
+        print("  ",terminator:"")
     }
 
 }
 
 // Row complete... go to the next line
-print("")
+    
+    // the \n character sequence
+    // pushes content down to the next line
+    
+    print("\n", terminator: "")
 
+}
+
+for _ in 1...1 {
+    print("*******", terminator:"")
+    print("\n", terminator: "")
+}
+
+
+for _ in 1...h{
+    print("   *   ")
+    print("\n", terminator: "")
 }
 // Produce top of trident according to length given
 //for _ in 1...t {
