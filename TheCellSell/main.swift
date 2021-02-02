2553c84cc865fb45bd90c4398a9202683285596d
Evan Armstrong <earmstrong@lcs.on.ca>
2021-02-01 21:16:25 -0500

    Added extension and more to the cellsell

--- TheCellSell/main.swift
+++ TheCellSell/main.swift
@@ -20,10 +20,34 @@ print("=============")
 
 // INPUT
 
-// Get daytime minutes
-print("Number of daytime minutes?")
-let day = Int(readLine()!)!
-
+var day = 0
+while true {
+    
+    // Prompt the user
+    print("Number of daytime minutes? ")
+    
+    // Collect input
+    guard let givenInput = readLine() else {
+        continue
+    }
+    
+    // Convert to an integer
+    guard let givenInteger = Int(givenInput) else {
+        continue
+    }
+    
+    // Check that the value is positive
+    guard givenInteger > 0 else {
+        continue
+    }
+    
+    // Assign the given integer to our "day" variable
+    day = givenInteger
+    
+    // Stop the loop
+    break
+    
+}
 // Get evening minutes
 print("Number of evening minutes?")
 let evening = Int(readLine()!)!
